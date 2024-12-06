29cb5acf1da021fec597bda5610a3bbfbccca9df
Adolfo David Romero <Romeroad@Sheridancollege.ca>
2024-12-06 08:31:55 -0500

    Initial Commit

---
+++ finalExamAdolfo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  finalExamAdolfo
+//
+//  Created by David Romero on 2024-12-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ finalExamAdolfo/finalExamAdolfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalExamAdolfoApp.swift
+//  finalExamAdolfo
+//
+//  Created by David Romero on 2024-12-06.
+//
+
+import SwiftUI
+
+@main
+struct finalExamAdolfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
