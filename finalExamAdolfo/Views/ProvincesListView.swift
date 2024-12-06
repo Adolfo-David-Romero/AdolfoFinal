2376f788ad59aacee17b91e88c0b89605b86cc86
Adolfo David Romero <Romeroad@Sheridancollege.ca>
2024-12-06 08:47:18 -0500

    Changed Target IOS to 17.0 and added empty views 1 and 2.

---
+++ finalExamAdolfo/Views/ProvincesListView.swift
@@ -0,0 +1,22 @@
+//
+//  ProvincesListView.swift
+//  finalExamAdolfo
+//
+//  Created by David Romero on 2024-12-06.
+//
+
+import SwiftUI
+
+struct ProvincesListView: View {
+    var body: some View {
+        NavigationStack{
+            Form{
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            }.navigationTitle("ProvincesListView")
+        }
+    }
+}
+
+#Preview {
+    ProvincesListView()
+}
